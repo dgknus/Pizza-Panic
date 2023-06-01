1505195b23cc1cebb484adf58329c7c92a129a28
ahmetcann0 <107917764+ahmetcann0@users.noreply.github.com>
2023-06-02 00:15:54 +0300

    Revert "Şifre yazarken tuşlar renk değiştiriyor"
    This reverts commit 692d2690bb8d8f47dfe2a99e9d52e49993eac460.

--- Assets/Scripts/CaseMechanism/Keypad.cs
+++ Assets/Scripts/CaseMechanism/Keypad.cs
@@ -5,59 +5,29 @@ using UnityEngine.UI;
 
 public class Keypad : MonoBehaviour
 {
-    [SerializeField] public Text Ans;
-    [SerializeField] private Button[] numberButtons;
+ 
+[SerializeField] public Text Ans;
 
-    public string Answer = "123456";
-    private ColorBlock[] defaultButtonColors;
-    public float colorChangeDuration = 0.2f;
+public string Answer = "123456";
 
-    private void Start()
-    {
-        // Store the default colors of the buttons
-        defaultButtonColors = new ColorBlock[numberButtons.Length];
-        for (int i = 0; i < numberButtons.Length; i++)
-        {
-            defaultButtonColors[i] = numberButtons[i].colors;
-        }
+public void Number(int number){
+
+    Ans.text += number.ToString();
+
+}
+
+public void Execute(){
+
+    if(Ans.text == Answer){
+
+         Ans.text = "Correct     ";
     }
+    else{
 
-    public void Number(int number)
-    {
-        Ans.text += number.ToString();
-        StartCoroutine(ChangeButtonColor(number));
+        Ans.text = "Invalid      ";
+        
     }
+}
 
-    public void Execute()
-    {
-        if (Ans.text == Answer)
-        {
-            Ans.text = "Correct     ";
-        }
-        else
-        {
-            Ans.text = "Invalid      ";
-        }
-    }
-
-    private IEnumerator ChangeButtonColor(int number)
-    {
-        // Get the index of the button based on the pressed number
-        int buttonIndex = number;
-
-        // Check if the button index is within the array bounds
-        if (buttonIndex >= 0 && buttonIndex < numberButtons.Length)
-        {
-            // Change the button color to red temporarily
-            ColorBlock colorBlock = numberButtons[buttonIndex].colors;
-            colorBlock.normalColor = Color.red;
-            numberButtons[buttonIndex].colors = colorBlock;
-
-            // Wait for a short duration
-            yield return new WaitForSeconds(colorChangeDuration);
-
-            // Restore the button color to the default
-            numberButtons[buttonIndex].colors = defaultButtonColors[buttonIndex];
-        }
-    }
+    
 }
